2ab329a7c99672fe67d43e019493fa6a71a25887
epersonf <epersoncmxf@gmail.com>
2020-08-30 01:56:00 -0300

    Adicionado cors ao header default

--- app.js
+++ app.js
@@ -14,6 +14,13 @@ const urlEncondedParser = bodyParser.urlencoded({ extended: false });
 app.use(jsonParser);
 app.use(urlEncondedParser);
 
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+    next();
+});
+
 mainPath(app);
 loginPath(app);
 
